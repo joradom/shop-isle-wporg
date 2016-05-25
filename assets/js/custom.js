56f6814473d0b0f4c5a9200ed007dd2e1e266c77
rodica-andronache <rodik_andronache@yahoo.com>
2016-05-25 15:24:12 +0300

    !!! #14 and #13 checkboxes issues with Jetpack subscribtions module

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -354,7 +354,7 @@
 	$("#review_form form :input").each(function(index, elem) {
 		var eId = $(elem).attr("id");
 		var label = null;
-		if (eId && (label = $(elem).parents("form").find("label[for="+eId+"]")).length == 1) {
+		if (eId && (eId != 'subscribe_blog') && (label = $(elem).parents("form").find("label[for="+eId+"]")).length == 1) {
 			$(elem).attr("placeholder", $(label).text());
 			$(label).remove();
 		}
