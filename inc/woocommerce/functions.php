a1de600074247941c83e2c4dc7d01ad7f79082eb
Buruiana Claudiu <claudiu.buruiana@vertistudio.com>
2016-08-25 11:41:47 +0300

    #47 Display category description on all category pages

--- inc/woocommerce/functions.php
+++ inc/woocommerce/functions.php
@@ -463,3 +463,14 @@ if ( !function_exists( 'shop_isle_search_products_no_results_wrapper_end' ) ) {
 		}
 	}
 }	
+
+/* Display WooCommerce product category description on all category archive pages */
+function shop_isle_woocommerce_taxonomy_archive_description() {
+    if ( is_tax( array( 'product_cat', 'product_tag' ) ) && get_query_var( 'paged' ) != 0 ) {
+        $description = wc_format_content( term_description() );
+        if ( $description ) {
+            echo '<div class="term-description">' . $description . '</div>';
+        }
+    }
+}
+add_action( 'woocommerce_archive_description', 'shop_isle_woocommerce_taxonomy_archive_description');
