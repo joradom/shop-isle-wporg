89edc97a0db2baa7d3663cad247d33caa5fedabf
claudiuvertistudio <claudiu.buruiana@codeinwp.com>
2016-08-30 14:56:36 +0300

    #42 Contact form zoom in on mobile in issue

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
 <html <?php language_attributes(); ?> <?php shop_isle_html_tag_schema(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
